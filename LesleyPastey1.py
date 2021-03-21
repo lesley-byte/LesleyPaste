b198ce8fed9a584c77791c7e435a63c4c7f4b4f7
lesley-byte <60296103+lesley-byte@users.noreply.github.com>
2021-03-21 13:48:02 -0400

    Update LesleyPastey1.py
    once the .ahk file is launched, you will have to hold ctrl while pressing the letter you have selected

--- LesleyPastey1.py
+++ LesleyPastey1.py
@@ -2,8 +2,8 @@ import PySimpleGUI as sg
 
 sg.theme('DarkAmber')
 
-layout = [ [sg.Text("Text to paste from hotkeyb.ahk")],
-           [sg.Text("Enter text for ctrl-a"), sg.InputText()],
+layout = [ [sg.Text("Letter to paste to"), sg.InputText()],
+           [sg.Text("Enter text for letter"), sg.InputText()],
            [sg.Button('Ok'), sg.Button('Cancel')]]
 
 window = sg.Window('Lesley Pastey', layout)
@@ -12,13 +12,17 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel':
         break
-    print ('you entered', values[0])
+    print ('you entered', values[1])
     file1 = open(r"C:\Users\phosp\hotkeyb.ahk", "w+")
-    L = ["^a:: \n", "Send, "]
-    s = values[0]
+    L0 = ["^"]
+    L1 = values[0]
+    L2 = [":: \n", "Send, "]
+    s = values[1]
     s2 = " \n"
     s3 = "return"
-    file1.writelines(L)
+    file1.writelines(L0)
+    file1.writelines(L1)
+    file1.writelines(L2)
     file1.write(s)
     file1.write(s2)
     file1.write(s3)
@@ -28,5 +32,3 @@ while True:
     print(file1.read())
     file1.close()
 window.close()
-
-
